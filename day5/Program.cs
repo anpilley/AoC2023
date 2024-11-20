d2a8f082b6b7df75e0c1e34ea1ce251679584d25
Andrew Pilley <anpilley@mycronite.net>
2024-11-20 16:54:57 +1100

    day 5, unstarted.

---
+++ day5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace day5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var regex = new Regex("(?<data>[0-9]+)|(?<header>[\\w+\\-]+ map:)|(?<seed>seeds:)");
+    }
+}
